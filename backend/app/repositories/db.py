a66cbd010392041850db65b8a2ad7253795dc5b4
kaankk5 <kaannkosti@gmail.com>
2024-03-23 02:20:32 +0100

    initialize db

--- backend/app/repositories/db.py
+++ backend/app/repositories/db.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from backend.config import Config
+from backend.config import Settings
 
 # Create SQLAlchemy engine
-engine = create_engine(Config.SQLALCHEMY_DATABASE_URL)
+engine = create_engine(Settings.SQLALCHEMY_DATABASE_URL)
 
 # Create a SessionLocal class for creating session objects
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

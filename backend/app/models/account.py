bcb6733405df4a841115700f795f356941b3f9cd
kaankk5 <kaannkosti@gmail.com>
2024-03-23 02:19:58 +0100

    create models account

--- backend/app/models/account.py
+++ backend/app/models/account.py
@@ -9,6 +9,8 @@ class Account(Base):
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     balance = Column(Float, default=0.0)
+    total_roi = Column(Float, nullable=True, default=0.0)
+    total_profit = Column(Float, nullable=True, default=0.0)
     trades = relationship("Trade", back_populates="account")
     user = relationship("User", back_populates="accounts")
 

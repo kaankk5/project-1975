5e4a19106a16bdddf366f6280f90c2bf58fca151
kaankk5 <kaannkosti@gmail.com>
2024-03-23 02:18:26 +0100

    create trade models

--- backend/app/models/trade.py
+++ backend/app/models/trade.py
@@ -1,6 +1,12 @@
 from base import Base
-from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
+from enum import Enum
+
+class LimitOrderType(str,Enum):
+    LIMIT_BUY ='LIMIT_BUY'
+    LIMIT_SELL ='LIMIT_BUY'
+
 
 
 class Trade(Base):
@@ -9,8 +15,14 @@ class Trade(Base):
     id = Column(Integer, primary_key=True)
     account_id = Column(Integer, ForeignKey('accounts.id'), nullable=False)
     symbol = Column(String, nullable=False)
-    quantity = Column(Float, nullable=False)
-    price = Column(Float, nullable=False)
+    leverage = Column(Integer, nullable=False)
+    buying_price = Column(Float, nullable=True)
+    selling_price = Column(Float, nullable=True)
+    profit = Column(Float, nullable=True)
+    current_price = Column(Float, nullable=True)
+    roi = Column(Float, nullable=True)
     timestamp = Column(DateTime, nullable=False)
-    profit = Column(Float, nullable=False)
+    is_long = Column(Boolean, nullable=False)
+    is_short = Column(Boolean, nullable=False)
     account = relationship("Account", back_populates="trades")
+

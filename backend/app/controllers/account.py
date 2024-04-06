e740ae3cbe7429447a629128ec7be982f38a52cb
kaankk5 <kaannkosti@gmail.com>
2024-04-06 02:53:22 +0200

    created controller trade

---
+++ backend/app/controllers/account.py
@@ -0,0 +1,6 @@
+
+
+
+class AccountController:
+    pass
+
--- backend/app/controllers/trade.py
+++ backend/app/controllers/trade.py
@@ -1,24 +1,14 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.ext.asyncio import AsyncSession
-from app.schemas.user import UserCreate, User
-from app.repositories.db import get_db_session
-from app.utils import hash_password, verify_password, set_password
+from fastapi import APIRouter
+from app.services.trade import TradeService
+from app.services.authentication import AuthService
 
 
-# router = APIRouter(prefix='/auth')
-#
-#
-# class TradeController:
-#     def __init__(self, trade_service: trade_service, db: AsyncSession):
-#         self.trade_service = trade_service
-#         self.db = db
-#
-#
-#     @router.post("/")
-#     async def open_trade(self, user_data: UserCreate, db: AsyncSession = Depends(get_db_session)):
-#         pass
-#
-#         # return await self.user_service.create_user(user_data, db)
-# #        Status code
-#
+class TradeController:
+
+    def __init__(self, trade_service: TradeService, auth_service: AuthService):
+        self.trade_service = trade_service
+        self.auth_service = auth_service
+        self.router = APIRouter()
+        self.add_routes()
+
 
